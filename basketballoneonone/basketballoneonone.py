f8efc65aecaea482ec26cb79ae5f63670d003ef2
christianfelt <christianfelt@comcast.net>
2019-12-20 11:39:04 -0700

    Basketball finished.

--- basketballoneonone/basketballoneonone.py
+++ basketballoneonone/basketballoneonone.py
@@ -3,3 +3,16 @@ Christian Felt
 December 19, 2019
 Solves the 'Basketball One on One' problem on Kattis.
 """
+
+score_log = input()
+score_dict = {'A': 0, 'B': 0}
+for i in range(0,len(score_log),2):
+    letter = score_log[i]
+    assert letter == 'A' or letter == 'B'
+    score = int(score_log[i+1])
+    score_dict[letter] += score
+if score_dict['A'] > score_dict['B']:
+    print('A')
+else:
+    print('B')
+
