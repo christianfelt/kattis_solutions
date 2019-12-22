5e497dc70c5f8f884a44d6438e11fdfbc03e3a58
christianfelt <christianfelt@comcast.net>
2019-12-22 14:33:47 -0700

    Effectless dynamic programming removed. Tests improved. All works, but time limit exceeded.

--- zero_one_sequences/zero_one_sequences.py
+++ zero_one_sequences/zero_one_sequences.py
@@ -6,25 +6,19 @@ Solves the Kattis problem "0-1 Sequences"
 import time
 
 MODULUS = 1000000007
-known_sols = {}
 known_chunks = {}
 NUM_CHUNKS = 100
 
 
 def get_num_inversions(s):
-    if s in known_sols:
-        return known_sols[s]
-    else:
-        num_inversions = 0
-        num_zeros = s.count('0')
-        for char in s:
-            if char == '1':
-                num_inversions += num_zeros
-            if char == '0':
-                num_zeros -= 1
-        this_sol = num_inversions % MODULUS
-        known_sols[s] = this_sol
-        return this_sol
+    num_inversions = 0
+    num_zeros = s.count('0')
+    for char in s:
+        if char == '1':
+            num_inversions += num_zeros
+        if char == '0':
+            num_zeros -= 1
+    return num_inversions % MODULUS
 
 
 def get_all_combs(chunk):
@@ -53,14 +47,14 @@ def add_new_combs_to_k_strings(two_k_strings, chunk):
 
 
 def make_two_k_strings(s):
-    two_k_strings = []
+    two_k_strings = set()
     k = s.count('?')
     for i in range(2 ** k):
         this_perm = bin(i)[2:].zfill(k)  # Strip 0b prefix off binary number.
         this_s = s
         for j in range(k):
             this_s = this_s.replace('?', this_perm[j], 1)
-        two_k_strings.append(this_s)
+        two_k_strings.add(this_s)
     return two_k_strings
 
 
