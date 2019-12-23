322419e915a425240a09e8ff4b82a4412f4fafe0
christianfelt <christianfelt@comcast.net>
2019-12-23 11:07:37 -0700

    Three D Printed Statues solved.

--- three_D_printed_statues/three_D_printed_statues.py
+++ three_D_printed_statues/three_D_printed_statues.py
@@ -3,3 +3,11 @@ Christian Felt
 December 22, 2019
 Solves the 3D Printed Statues problem on Kattis.
 """
+
+statues_ordered = int(input())
+production_max = 1
+days_elapsed = 1
+while production_max < statues_ordered:
+    production_max = production_max * 2
+    days_elapsed += 1
+print(days_elapsed)
