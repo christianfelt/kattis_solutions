024e64a59af5aaa4170f0c24177709af0ea4a0cf
christianfelt <christianfelt@comcast.net>
2019-12-28 09:16:59 -0700

    4 Thought brute force draft 1

--- four_thought/four_thought.py
+++ four_thought/four_thought.py
@@ -3,3 +3,25 @@ Christian Felt
 December 2019
 Solves the Kattis problem "4 Thought"
 """
+
+OPS = {"+", "-", "*", "/"}
+
+
+def solve(n):
+    for op0 in OPS:
+        for op1 in OPS:
+            for op2 in OPS:
+                this_sol_str = "4 " + op0 + " 4 " + op1 + " 4 " + op2 + " 4"
+                if eval(this_sol_str) == n:
+                    return this_sol_str + " = " + str(n)
+    return "no solution"
+
+
+def main():
+    m = int(input())
+    for i in range(m):
+        print(solve(int(input().strip())))
+
+
+if __name__ == "__main__":
+    main()
