93307ae8debcd2839ab384f75086c4b3d454501b
christianfelt <christianfelt@comcast.net>
2019-12-30 17:07:20 -0700

    Aaah! finished.

--- aaah/aaah.py
+++ aaah/aaah.py
@@ -3,3 +3,9 @@ Christian Felt
 December 2019
 Solves the "Aaah!" problem on Kattis
 """
+doctor = input()
+jon = input()
+if doctor.count("a") >= jon.count("a"):
+    print("go")
+else:
+    print("no")
