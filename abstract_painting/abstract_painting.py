ae8646106b7acec0843f58883be46fa51cce2551
christianfelt <christianfelt@comcast.net>
2020-01-04 17:09:43 -0700

    Abstract Painting correct but time limit exceeded.

--- abstract_painting/abstract_painting.py
+++ abstract_painting/abstract_painting.py
@@ -3,6 +3,7 @@ Christian Felt
 January 2020
 Solves the "Abstract Painting" problem on Kattis.
 """
+from itertools import combinations_with_replacement, permutations
 
 COLORS = {"red", "green", "blue"}
 
@@ -33,6 +34,25 @@ for color1 in COLORS:
             VALID_SQUARES.append(Square(color1, color2, color1, color2))
 
 
+def make_paintings(R, C):
+    square_indices = range(0, len(VALID_SQUARES))
+    c = combinations_with_replacement(square_indices, R * C)
+    perms = []
+    for comb in c:
+        perms.extend(list(permutations(comb)))
+    perms = set(perms)  # Get rid of duplicates.
+    perms_grids = []
+    for q, p in enumerate(perms):
+        k = 0
+        perms_grids.append([])
+        for i in range(R):
+            perms_grids[q].append([])
+            for j in range(C):
+                perms_grids[q][i].append(VALID_SQUARES[p[k]])
+                k += 1
+    return perms_grids
+
+
 def is_valid_painting(grid, R, C):
     for i in range(R):
         for j in range(C):
@@ -57,88 +77,15 @@ def is_valid_painting(grid, R, C):
     return True
 
 
-# def valid_add(grid, square, row, col):
-#     if len(grid[row]) == 0 and row == 0 and col == 0:
-#         grid[row].append(square)
-#         return 1
-#     else:
-#         grid[row].append(square)
-#         assert grid[row][col] == square
-#         is_valid = True
-#         if len(grid) > row + 1 and len(grid[row + 1]) > col:  # Check below
-#             this_square = grid[row + 1][col]
-#             if this_square.top_edge != square.bottom_edge:
-#                 is_valid = False
-#         if row > 0 and is_valid:
-#             this_square = grid[row - 1][col]  # Check above
-#             if this_square.bottom_edge != square.top_edge:
-#                 is_valid = False
-#         if col > 0 and is_valid:  # Check left
-#             this_square = grid[row][col - 1]
-#             if this_square.right_edge != square.left_edge:
-#                 is_valid = False
-#         if not is_valid:
-#             grid[row].pop()
-#             return 0
-#         else:
-#             return 1
-
 def brute_force(R, C):
     """Make all possible paintings then count which ones are good."""
-    paintings = []
-    for q in range(R * C * 18):
-        paintings.append([])
-        for i in range(R):
-            paintings[q].append([])
-            for j in range(C):
-                for valid_square in VALID_SQUARES:
-                    paintings[q][i].append(valid_square)
+    paintings = make_paintings(R, C)
     num_good_paintings = 0
     for painting in paintings:
         if is_valid_painting(painting, R, C):
             num_good_paintings += 1
     return num_good_paintings
 
-
-# def one_by_two():
-#     good_paintings = []
-#
-#     for valid_square1 in VALID_SQUARES:
-#         painting = []
-#         valid_add(painting, valid_square1, 0, 0)
-#         for valid_square2 in VALID_SQUARES:
-#             valid_add(painting, valid_square2, 0, 1)
-#
-#     grid = [[]]
-#     valid_add(grid, VALID_SQUARES[0], 0, 0)
-#     valid_add(grid, VALID_SQUARES[0], 0, 1)
-#     print(grid)
-#     # for valid_square in VALID_SQUARES:
-#     #     valid_add(grid, valid_square,0,0)
-#
-#
-# one_by_two()
-
-# def brute_force(R, C):
-#     good_paintings = set()
-#     dead_ends = set()
-#     num_good_paintings = 0
-#     grids = []
-#     for i in range(R):
-#         grids.append([])
-#         for j in range(C):
-#             for valid_square in VALID_SQUARES:
-#                 if is_valid_add(grid, valid_square):
-#                     grid[i].append(valid_square)
-#                     break
-
-
-# pass
-#  Make square
-#  Add square to grid
-#  Check whether all requirements are met
-#  If yes, keep going
-#  If no, add current grid to list of invalid beginnings.
 
 T = int(input())
 for i in range(T):
